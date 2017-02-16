45353a8d91e6aca9020c555f0d37991d37af0ec9
sethulekshmi <sethulekshmi777@gmail.com>
2017-02-16 22:06:52 +0530

    Update assets_popup_functions.js

--- Client_Side/JavaScript/page_functions/assets_popup_functions.js
+++ Client_Side/JavaScript/page_functions/assets_popup_functions.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 
-	$('#addToAssets').click(function(){
+	$('#addToVhcls').click(function(){
 	
 		setCookie();
 		$("#vhclsTbl".html('<tr><td style="text-align:center;"><img src="Images/'+pgNm+'/loading.gif" height="50" width="50" alt="loading" text="please wait..." /><br /><i>Found: <span class="numFound">0 assets</span></i></td></tr>');
@@ -23,7 +23,7 @@ $(document).ready(function(){
 			{
 				var exists = false;
 				var rw = $(this);
-				$('.selassetRw').each(function(){
+				$('.selVehRw').each(function(){
 					if($(this).find('.assetID').val() == $(rw).find('.assetID').val())
 					{
 						exists = true;
@@ -34,7 +34,7 @@ $(document).ready(function(){
 					$(this).find('.isChk').val('false')
 					$(this).find('.chkBx').css('background-image','url("")')
 					var nwRw = $(this).html().replace('<span class="chkSpc"></span><span style="background-image: url(&quot;&quot;);" class="chkBx"></span><input class="isChk" value="false" type="hidden">', '<span onclick="removeRowFromSelectedAssets(this)" style="float:right; margin-right:10px; cursor:pointer;"><img src="Icons/'+pgNm+'/minus.svg" width="15" /></span>')
-					$("#selVhclsTbl").append('<tr class="selassetRw">' + nwRw + '</tr>');
+					$("#selVhclsTbl").append('<tr class="selVehRw">' + nwRw + '</tr>');
 				}
 			}
 		})
@@ -44,7 +44,7 @@ $(document).ready(function(){
 	});
 })
 
-function removeRowFromSelectedAssets(el)
+function removeRowFromSelectedVehicles(el)
 {
 	$(el).parent().parent().remove();
 	$('#selVhclsTbl').parent().css('margin-top','0px');
