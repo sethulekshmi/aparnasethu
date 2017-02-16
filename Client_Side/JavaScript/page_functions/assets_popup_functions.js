4225270006d1cd4d08125eb205bf68b4820da75e
sethulekshmi <sethulekshmi777@gmail.com>
2017-02-16 15:17:30 +0530

    Update assets_popup_functions.js

--- Client_Side/JavaScript/page_functions/assets_popup_functions.js
+++ Client_Side/JavaScript/page_functions/assets_popup_functions.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
 	$('#addToAssets').click(function(){
 	
 		setCookie();
-		$("#assetsTbl").html('<tr><td style="text-align:center;"><img src="Images/'+pgNm+'/loading.gif" height="50" width="50" alt="loading" text="please wait..." /><br /><i>Found: <span class="numFound">0 assets</span></i></td></tr>');
+		$("#vhclsTbl".html('<tr><td style="text-align:center;"><img src="Images/'+pgNm+'/loading.gif" height="50" width="50" alt="loading" text="please wait..." /><br /><i>Found: <span class="numFound">0 assets</span></i></td></tr>');
 		$('#chooseOptTbl').fadeIn(1000);
 		$('#fade').fadeIn(1000);
 		loadAssets();
@@ -34,7 +34,7 @@ $(document).ready(function(){
 					$(this).find('.isChk').val('false')
 					$(this).find('.chkBx').css('background-image','url("")')
 					var nwRw = $(this).html().replace('<span class="chkSpc"></span><span style="background-image: url(&quot;&quot;);" class="chkBx"></span><input class="isChk" value="false" type="hidden">', '<span onclick="removeRowFromSelectedAssets(this)" style="float:right; margin-right:10px; cursor:pointer;"><img src="Icons/'+pgNm+'/minus.svg" width="15" /></span>')
-					$("#selAssetsTbl").append('<tr class="selassetRw">' + nwRw + '</tr>');
+					$("#selVhclsTbl").append('<tr class="selassetRw">' + nwRw + '</tr>');
 				}
 			}
 		})
@@ -47,7 +47,7 @@ $(document).ready(function(){
 function removeRowFromSelectedAssets(el)
 {
 	$(el).parent().parent().remove();
-	$('#selAssetsTbl').parent().css('margin-top','0px');
-	$('#selAssetsTbl').parent().parent().siblings('.scrlHldr').children('.scrlBr').css('top', "0px")
+	$('#selVhclsTbl').parent().css('margin-top','0px');
+	$('#selVhclsTbl').parent().parent().siblings('.scrlHldr').children('.scrlBr').css('top', "0px")
 	changeBarSize();
 }
